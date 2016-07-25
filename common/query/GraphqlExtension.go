b8c4eec6d4aad7126ceab33712a3c96ba2a31360
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-07-25 13:03:37 +1200

    Extension added for graphql golang project to support key value pair of value type equal to sting

---
+++ common/query/GraphqlExtension.go
@@ -0,0 +1,18 @@
+package query
+
+import "github.com/graphql-go/graphql"
+
+type StringKeyValue struct {
+	Key   string `json:"id"`
+	Value string `json:"name"`
+}
+
+var StringKeyValuePairType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "StringKeyValue",
+		Fields: graphql.Fields{
+			"key":   &graphql.Field{Type: graphql.String},
+			"value": &graphql.Field{Type: graphql.String},
+		},
+	},
+)
