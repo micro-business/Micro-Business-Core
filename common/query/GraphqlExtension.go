2dd1e669431bb0f77f15c2961df735896be8b0af
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-10-13 23:02:03 +1300

    Graphql Extension enhanced, added method to return selected fields in a graphql query

--- common/query/GraphqlExtension.go
+++ common/query/GraphqlExtension.go
@@ -1,6 +1,9 @@
 package query
 
-import "github.com/graphql-go/graphql"
+import (
+	"github.com/graphql-go/graphql"
+	ast "github.com/graphql-go/graphql/language/ast"
+)
 
 type StringKeyValuePair struct {
 	Key   string `json:"key"`
@@ -16,3 +19,30 @@ var StringKeyValuePairType = graphql.NewObject(
 		},
 	},
 )
+
+func GetSelectedFields(selectionPath []string,
+	resolveParams graphql.ResolveParams) []string {
+	fields := resolveParams.Info.FieldASTs
+	for _, propName := range selectionPath {
+		found := false
+		for _, field := range fields {
+			if field.Name.Value == propName {
+				selections := field.SelectionSet.Selections
+				fields = make([]*ast.Field, 0)
+				for _, selection := range selections {
+					fields = append(fields, selection.(*ast.Field))
+				}
+				found = true
+				break
+			}
+		}
+		if !found {
+			return []string{}
+		}
+	}
+	var collect []string
+	for _, field := range fields {
+		collect = append(collect, field.Name.Value)
+	}
+	return collect
+}
